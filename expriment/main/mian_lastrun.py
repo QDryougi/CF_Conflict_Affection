829cc9cf0f5ec2d492819cd2673f83ee589e0459
QDryougi <596195788@qq.com>
2024-04-14 17:30:10 +0800

    完成文档编写，迁移项目分析至文档

--- expriment/main/mian_lastrun.py
+++ expriment/main/mian_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.2.3),
-    on April 14, 2024, at 15:01
+    on April 14, 2024, at 16:29
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
